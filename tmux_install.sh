a37c9b8bbf64cd30cb01df888aaa216bea6f9005
Steini UNC <steiniunc@kria.local>
2024-09-28 19:36:32 -0400

    Fixed issue with sourcing the tmux conf

--- tmux_install.sh
+++ tmux_install.sh
@@ -5,5 +5,8 @@ stow -t ~ tmux
 # Clone TPM manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Source the tmux configuration
+tmux source-file ~/.tmux.conf
+
 # Install script for installing the plugins for tmux
 bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
